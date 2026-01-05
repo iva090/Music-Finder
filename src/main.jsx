9a1cb86fe9a5618191ba3b81b24ab0e198092c3c
Iva Mchedlishvili <ivamchd09@gmail.com>
2026-01-05 21:01:35 +0400

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; 
 import "./index.css";
 import App from "./App.jsx";
 
@@ -8,7 +8,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
+  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
+  </React.StrictMode>
 );
