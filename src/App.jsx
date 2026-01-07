18686940d64276edc7bf470430e20f3fc047fe15
Iva Mchedlishvili <ivamchd09@gmail.com>
2026-01-07 16:41:10 +0400

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,53 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Mainpage from './pages/Mainpage';
-import Favorites from './pages/favorites';
-import About from './pages/About'
-import Header from './components/headers/Header'
-import Contact from './pages/Contact';
-import { FavoritesContext } from './components/FavoritesContext';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Mainpage from "./pages/Mainpage";
+import Favorites from "./pages/Favorites";
+import About from "./pages/About";
+import Header from "./components/headers/Header";
+import Contact from "./pages/Contact";
+import { FavoritesContext } from "./components/FavoritesContext";
+import { useEffect, useState } from "react";
 
 function App() {
   const [favorites, setFavorites] = useState(() => {
-    const saved = localStorage.getItem('favoriteSongs');
-    return saved ? JSON.parse(saved) : [];
+    if (typeof window !== "undefined") {
+      const saved = localStorage.getItem("favoriteSongs");
+      return saved ? JSON.parse(saved) : [];
+    }
+    return [];
   });
 
   useEffect(() => {
-    localStorage.setItem('favoriteSongs', JSON.stringify(favorites));
+    localStorage.setItem("favoriteSongs", JSON.stringify(favorites));
   }, [favorites]);
 
-  function addToFavorites(song){
-    if (favorites.some((sg) => sg.id === song.id)){
-      alert("Already in favorites")
+  function addToFavorites(song) {
+    if (favorites.some((sg) => sg.id === song.id)) {
+      alert("Already in favorites");
     } else {
       setFavorites((prev) => [...prev, song]);
     }
-  };
+  }
 
   function removeFromFavorites(id) {
     setFavorites((prev) => prev.filter((song) => song.id !== id));
   }
 
   return (
-    <FavoritesContext.Provider value={{favorites, addToFavorites, removeFromFavorites}}>
-        <Header />
-        <div>
-          <Routes>
-            <Route path="/" element={<Mainpage />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+    <FavoritesContext.Provider
+      value={{ favorites, addToFavorites, removeFromFavorites }}
+    >
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </FavoritesContext.Provider>
-  )
+  );
 }
 
 export default App;
