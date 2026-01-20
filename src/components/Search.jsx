4f3332c5f14f2b50df9ec6c200549bc34db22a73
Iva Mchedlishvili <ivamchd09@gmail.com>
2026-01-20 12:51:15 +0400

    changed search bar to lag less

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
 import { SearchIcon } from 'lucide-react';
 
 function Search({ onSearch }) {
+    const [query, setQuery] = useState("");
 
-    const handleInputChange = (e) => {
-        const valu = e.target.value;
-        onSearch(valu);
-    }
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        onSearch(query);
+    };
 
     return (
-        <div className="sticky input-group h-10 w-full">
-            <input className="text-xl ig-input text-center" placeholder='Enter a song' type="text" onChange={handleInputChange}/>
-        </div>
+        <form 
+            onSubmit={handleFormSubmit} 
+            className="sticky input-group h-10 w-full flex items-center gap-2"
+        >
+            <div className="relative flex-1">
+                <input 
+                    className="text-xl pl-12  ig-input text-center w-full h-full" 
+                    placeholder='Enter a song' 
+                    type="text" 
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+            </div>
+            
+            <button 
+                type="submit" 
+                className="p-2 bg-purple-800 text-white rounded-md hover:bg-blue-600"
+            >
+                <SearchIcon size={20} />
+            </button>
+        </form>
     )
 }
 
